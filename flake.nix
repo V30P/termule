ef21ddba1106e2b897e4c68b06fe988b614a91f1
V30P <nhenning06@gmail.com>
2026-07-26 10:26:03 -0400

    Remove dependency on SharpHook

--- flake.nix
+++ flake.nix
@@ -16,27 +16,12 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
-        # These are needed for SharpHook's libUIOHook dependency
-        runtimeDeps = [
-          pkgs.libX11
-          pkgs.libXtst
-          pkgs.libXt
-          pkgs.libXinerama
-        ];
       in
       {
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.dotnet-sdk_10
           ];
-
-          # Make sure libUIOHook can find its dependencies
-          shellHook = ''
-            ALL_PATHS="${pkgs.lib.makeLibraryPath runtimeDeps}"
-            export LD_LIBRARY_PATH="$ALL_PATHS:$LD_LIBRARY_PATH"
-            export NIX_LD_LIBRARY_PATH="$ALL_PATHS:$NIX_LD_LIBRARY_PATH"
-          '';
         };
       }
     );
