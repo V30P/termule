99139d10700d2478de9215b085022d2d81237862
V30P <nhenning06@gmail.com>
2026-08-27 12:21:52 -0400

    Update ResourceLoader tests

--- Tests/Systems/Resources/TestResourceLoader.cs
+++ Tests/Systems/Resources/TestResourceLoader.cs
@@ -1,4 +1,4 @@
-using System.IO.Abstractions.TestingHelpers;
+using System.Text;
 using Termule.Engine.Exceptions;
 using Termule.Engine.Systems.Resources;
 
@@ -7,44 +7,18 @@ namespace Termule.Tests.Systems.Resources;
 public class TestResourceLoader
 {
     [Fact]
-    public void Load_CachesValuesAndPullFromCache()
+    public void Load_LoadsResource()
     {
-        MockFileSystem fileSystem = new(new Dictionary<string, MockFileData>
+        FakeResourceProvider provider = new()
         {
-            { "/test.fake", new MockFileData(Serializer.Serialize(new FakeResource("Test"))) }
-        });
-        ResourceLoader resourceLoader = new(fileSystem, "/");
-
-        _ = resourceLoader.Load<FakeResource>("test");
-        fileSystem.RemoveFile("/test.fake");
-        FakeResource loaded = resourceLoader.Load<FakeResource>("test");
-
-        Assert.Equivalent(new FakeResource("Test"), loaded);
-    }
-
-    [Fact]
-    public void Load_GivenPathWithoutExtension_ReturnsResource()
-    {
-        MockFileSystem fileSystem = new(new Dictionary<string, MockFileData>
-        {
-            { "/test.fake", new MockFileData(Serializer.Serialize(new FakeResource("Test"))) }
-        });
-        ResourceLoader resourceLoader = new(fileSystem, "/");
-
-        FakeResource loaded = resourceLoader.Load<FakeResource>("test");
-
-        Assert.Equivalent(new FakeResource("Test"), loaded);
-    }
-
-    [Fact]
-    public void Load_RespectsResourceDir()
-    {
-        MockFileSystem fileSystem = new(new Dictionary<string, MockFileData>
-        {
-            { "/test.fake", new MockFileData(Serializer.Serialize(new FakeResource("Wrong"))) },
-            { "/dir/test.fake", new MockFileData(Serializer.Serialize(new FakeResource("Correct"))) }
-        });
-        ResourceLoader resourceLoader = new(fileSystem, "/dir", false);
+            Resources = new()
+            {
+                { "TEST", Serializer.Serialize(new FakeResource("Wrong")) },
+                { "test", Serializer.Serialize(new FakeResource("Correct")) },
+                { "test1", Serializer.Serialize(new FakeResource("Wrong")) }
+            }
+        };
+        ResourceLoader resourceLoader = new(provider);
 
         FakeResource loaded = resourceLoader.Load<FakeResource>("test");
 
@@ -52,45 +26,47 @@ public class TestResourceLoader
     }
 
     [Fact]
-    public void Load_WhenPathDoesNotExist_Throws()
+    public void Load_CachesAndPullsFromCache()
     {
-        ResourceLoader resourceLoader = new(new MockFileSystem(), "/");
+        FakeResourceProvider provider = new()
+        {
+            Resources = new() { { "test", Serializer.Serialize(new FakeResource("Test")) } }
+        };
+        ResourceLoader resourceLoader = new(provider);
+
+        _ = resourceLoader.Load<FakeResource>("test");
+        _ = provider.Resources.Remove("test");
+        FakeResource loaded = resourceLoader.Load<FakeResource>("test");
+
+        Assert.Equivalent(new FakeResource("Test"), loaded);
+    }
+
+    [Fact]
+    public void Load_WhenResourceDoesNotExist_Throws()
+    {
+        ResourceLoader resourceLoader = new(new FakeResourceProvider());
 
         _ = Assert.Throws<ResourceLoadException>(() => resourceLoader.Load<FakeResource>("test"));
     }
 
-    [Fact]
-    public void Load_WithFullPath_ReturnsResource()
-    {
-        MockFileSystem fileSystem = new(new Dictionary<string, MockFileData>
-        {
-            { "/test.fake", new MockFileData(Serializer.Serialize(new FakeResource("Test"))) }
-        });
-        ResourceLoader resourceLoader = new(fileSystem, "/");
-
-        FakeResource loaded = resourceLoader.Load<FakeResource>("test.fake");
-
-        Assert.Equivalent(new FakeResource("Test"), loaded);
-    }
-
-    [Fact]
-    public void Load_WithMultilevelPath_ReturnsResource()
-    {
-        MockFileSystem fileSystem = new(new Dictionary<string, MockFileData>
-        {
-            { "/dir1/dir2/test.fake", new MockFileData(Serializer.Serialize(new FakeResource("Test"))) }
-        });
-        ResourceLoader resourceLoader = new(fileSystem, "/");
-
-        FakeResource loaded = resourceLoader.Load<FakeResource>("dir1/dir2/test");
-
-        Assert.Equivalent(new FakeResource("Test"), loaded);
-    }
-
     private sealed class FakeResource(string text) : IResource
     {
-        public static string FileExtension => ".fake";
-
         public string Text { get; set; } = text;
+    }
+
+    private sealed class FakeResourceProvider() : IResourceProvider
+    {
+        public Dictionary<string, string> Resources { get; init; } = [];
+
+        public bool ResourceExists(string name)
+        {
+            return Resources.ContainsKey(name);
+        }
+
+        public Stream GetResourceStream(string name)
+        {
+            byte[] resourceBytes = Encoding.UTF8.GetBytes(Resources[name]);
+            return new MemoryStream(resourceBytes);
+        }
     }
 }
